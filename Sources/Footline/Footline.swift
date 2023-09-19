0c48ec9a677356f17a9ccf130669b5b24ba45eab
John Behnke <john@behnke.xyz>
2023-09-19 12:42:12 -0400

    Updates for iOS 17

--- Sources/Footline/Footline.swift
+++ Sources/Footline/Footline.swift
@@ -10,25 +10,9 @@ import SwiftUI
 public struct Footline: View {
     @State private var isPressed: Bool = false
     @State private var numberOfPresses: Int = 0
-    @State private var symbolShadowColor: Color = .red
-    @State private var symbolForegroundGradient: LinearGradient =
-    LinearGradient(
-        colors: [.red, .pink],
-        startPoint: .top, endPoint: .bottom
-    )
+    @State private var symbolColor: Color = .red
     
-    let symbolFillGradients: [LinearGradient] = [
-        LinearGradient(colors: [.blue, .teal], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.orange, .yellow], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.green, .blue], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.pink, .orange], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.indigo, .blue], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.mint, .teal], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.purple, .red], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.blue, .mint], startPoint: .top, endPoint: .bottom),
-        LinearGradient(colors: [.red, .pink], startPoint: .top, endPoint: .bottom)
-    ]
-    let symbolShadowColors: [Color] = [
+    let symbolColors: [Color] = [
         .blue,
         .orange,
         .green,
@@ -74,32 +58,26 @@ public struct Footline: View {
                 Text("Made in ") + Text(self.locationName).bold() + Text(" with")
                 Image(systemName: symbolName)
                     .font(.caption2)
-                    .shadow(color: self.symbolShadowColor, radius: self.isPressed ? 3 : 1)
-                    .foregroundStyle(self.symbolForegroundGradient)
+                    .shadow(color: symbolColor, radius: self.isPressed ? 3 : 1)
+                    .foregroundStyle(symbolColor.gradient)
                     .scaleEffect(self.isPressed ? 0.75 : 1.0)
                     .rotationEffect(self.isPressed ? .degrees(numberOfPresses.isMultiple(of: 2) ? 10 : -10) : .degrees(0))
                     .animation(.spring(response: 0.2, dampingFraction: 0.1), value: self.isPressed)
                 
             }
+            .sensoryFeedback(.impact, trigger: isPressed)
             .onTapGesture {
-                self.hapticsGenerator.notificationOccurred(.success)
-                self.symbolForegroundGradient = symbolFillGradients[numberOfPresses % symbolFillGradients.count]
-                self.symbolShadowColor = symbolShadowColors[numberOfPresses % symbolShadowColors.count]
-                self.numberOfPresses += 1
-                self.isPressed.toggle()
+                symbolColor = symbolColors[numberOfPresses % symbolColors.count]
+                numberOfPresses += 1
+                isPressed.toggle()
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                    self.isPressed.toggle()
+                    isPressed.toggle()
                 }
             }
         }
     }
 }
 
-
-struct AppFooter_Previews: PreviewProvider {
-    static var previews: some View {
-        Footline(
-            locationName: "The Matrix"
-        )
-    }
+#Preview {
+    Footline(locationName: "The Matrix")
 }
